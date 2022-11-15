427dd53af1e142b8f76b6918c4c89c9ff73bfdd0
Miguel Angel Barahona <miguelbarahona00@gmail.com>
2022-11-14 21:27:00 -0500

    add home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,7 @@
 import 'dart:core';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:noteminticflutter/screens/newnotes_screen.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -10,36 +10,16 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromRGBO(91, 91, 90, 1),
-      body: Column(children: [
-        ElevatedButton(
+      body: Column(children: const [NewNotes()]),
+      floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
+      floatingActionButton: FloatingActionButton(
           onPressed: () {},
-          style: TextButton.styleFrom(
-              primary: Colors.white,
-              backgroundColor: const Color.fromRGBO(146, 146, 146, 1)),
-          child: Row(
-            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            // mainAxisSize: MainAxisSize.max,
-            children: [
-              const Text('Nota Nueva', style: TextStyle(fontSize: 24.0)),
-              const SizedBox(
-                width: 100,
-              ),
-              Container(
-                decoration: const BoxDecoration(
-                    color: Colors.yellow,
-                    borderRadius: BorderRadius.all(Radius.circular(10.0))),
-                width: 40.0,
-                height: 40.0,
-                child: const Icon(
-                  Icons.add,
-                  size: 24.0,
-                  color: Colors.black,
-                ),
-              ),
-            ],
-          ),
-        )
-      ]),
+          backgroundColor: Colors.yellow,
+          child: const Icon(
+            Icons.more_vert,
+            size: 35.0,
+            color: Colors.black,
+          )),
     );
   }
 }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -2,6 +2,7 @@ import 'dart:core';
 
 import 'package:flutter/material.dart';
 import 'package:noteminticflutter/screens/home_screen.dart';
+import 'package:noteminticflutter/screens/mytextfielpassword_screen.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({
@@ -143,65 +144,5 @@ class LoginScreen extends StatelessWidget {
             ]),
       ),
     );
-  }
-}
-
-class MyTextFielPassword extends StatefulWidget {
-  const MyTextFielPassword({super.key});
-
-  @override
-  State<MyTextFielPassword> createState() => _MyTextFielPassword();
-}
-
-class _MyTextFielPassword extends State<MyTextFielPassword> {
-  bool hidePassword = true;
-  @override
-  Widget build(BuildContext context) {
-    return TextFormField(
-        onChanged: (value) {
-          hidePassword = false;
-        },
-        controller: TextEditingController(text: "Vignesh123!"),
-        obscureText: hidePassword,
-        decoration: InputDecoration(
-          suffixIcon: IconButton(
-            color: Colors.black,
-            icon: hidePassword
-                ? const Icon(Icons.visibility_off)
-                : const Icon(Icons.visibility),
-            onPressed: () {
-              setState(() {
-                hidePassword = !hidePassword;
-              });
-            },
-          ),
-          focusedBorder: const OutlineInputBorder(
-              borderSide: BorderSide(
-                  width: 2.0, color: Color.fromRGBO(107, 106, 106, 1)),
-              borderRadius: BorderRadius.all(Radius.circular(10.0))),
-          filled: true,
-          fillColor: const Color.fromRGBO(107, 106, 106, 1),
-          hintText: 'Contraseña',
-          hintStyle: const TextStyle(fontSize: 18, color: Colors.white),
-          contentPadding:
-              const EdgeInsets.only(left: 15, top: 8, right: 15, bottom: 0),
-          enabledBorder: const OutlineInputBorder(
-              borderSide: BorderSide(
-                  width: 2.0, color: Color.fromRGBO(107, 106, 106, 1)),
-              borderRadius: BorderRadius.all(Radius.circular(10.0))),
-        ),
-        validator: (String? password) {
-          if (password == null || password.isEmpty) {
-            return 'Ingrese la contraseña';
-          } else {
-            String pattern =
-                r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$';
-            RegExp regExp = RegExp(pattern);
-            if (!regExp.hasMatch(password)) {
-              return 'La contraseña requiere una letra en mayuscula, un caracter especial y minimo de 8 caracteres.';
-            }
-          }
-          return null;
-        });
   }
 }
